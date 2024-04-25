56f7b1efb45e9573dfb1d49e7ce97feae257c2c7
Ekaterina Samsanovich <112251168+Katrysechka@users.noreply.github.com>
2024-04-25 13:30:31 +0300

    make info button

--- app_final.py
+++ app_final.py
@@ -10,9 +10,9 @@ from PyQt5.QtCore import Qt, QTimer, QPropertyAnimation, QEasingCurve, QUrl, QDi
 from PyQt5.QtGui import QPixmap, QPalette, QColor, QPainter, QBitmap, QIcon
 from PyQt5.QtWidgets import QApplication, QDialog, QStackedWidget
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QLabel, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QLabel, QMessageBox, QScrollArea
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QLabel, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QLabel, QMessageBox, QTabWidget, QWidget
 from PyQt5.QtCore import Qt, QTimer, QPropertyAnimation, QUrl, QEasingCurve, QDir
 from PyQt5.QtGui import QPixmap, QPalette, QColor
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
@@ -25,6 +25,7 @@ from PyQt5.QtCore import pyqtSignal
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QWidget
+from PyQt5.QtGui import QDesktopServices
 from PyQt5.QtGui import QPixmap
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QLineEdit, QLabel, QMessageBox
@@ -38,7 +39,6 @@ MUSIC = "./music_files/"
 FILES_UI= "./ui_files/"
 IMAGE = "./image_files/"
 
-# from entrance import LoginScreen, CreateAccScreen, RatingWindow
 
 class WelcomeScreen(QDialog):
     def __init__(self):
@@ -157,18 +157,87 @@ class CreateAccScreen(QMainWindow):
             conn.commit()
             conn.close()
 
-class RatingWindow(QMainWindow):
+class RatingWindow(QDialog):
     def __init__(self):
-        super().__init__()
+        super(RatingWindow, self).__init__()
+        self.setGeometry(350, 273, 400, 200)
         self.setWindowTitle("Info")
         layout = QVBoxLayout()
-        hello_label = QLabel("About us:", self)
-        layout.addWidget(hello_label)
 
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
+        hello_label = QLabel("Listen Your Emotion:", self)
+        hello_label.setAlignment(QtCore.Qt.AlignCenter)
+        hello_label.setStyleSheet("font-size: 20px; font-family: 'Snell Roundhand'; font-weight: bold;")
+        hello_label.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+    
+        layout.addWidget(hello_label)
         self.setWindowModality(Qt.ApplicationModal)
+        content_widget = QWidget()
+        
+        self.about_text_label = QLabel("")
+        self.about_text_label.setWordWrap(True) 
+        
+        # adding buttons
+        about_us_button = QPushButton("О нас")
+        chat_support_button = QPushButton("Чат с поддержкой")
+        rate_app_button = QPushButton("Оцените приложение")
+
+        about_us_button.setAutoDefault(False) 
+        chat_support_button.setAutoDefault(False)  
+        rate_app_button.setAutoDefault(False) 
+
+        # click-connect 
+        about_us_button.clicked.connect(self.show_about_us_text)
+        chat_support_button.clicked.connect(self.show_chat_support_text)
+        rate_app_button.clicked.connect(self.show_rate_app_text)
+        
+        # adding widgets into layout
+        layout.addWidget(about_us_button)
+        layout.addWidget(chat_support_button)
+        layout.addWidget(rate_app_button)
+        layout.addWidget(self.about_text_label)
+        self.setLayout(layout)
+
+        about_us_button.clicked.connect(lambda: self.set_button_color(about_us_button))
+        chat_support_button.clicked.connect(lambda: self.set_button_color(chat_support_button))
+        rate_app_button.clicked.connect(lambda: self.set_button_color(rate_app_button))
+        self.current_button = None
+
+    def set_button_color(self, button):
+        if self.current_button:
+            self.current_button.setStyleSheet("")
+            self.current_button.setStyleSheet("color: black;")
+        button.setStyleSheet("background-color: #778899; color: white;")
+        # update button
+        self.current_button = button
+        
+    def resizeEvent(self, event):
+        if self.size().height() == 500:
+            self.setGeometry(350, 273, 400, 500)  
+        elif self.size().height() == 100:
+            self.setGeometry(350, 273, 400, 100) 
+        elif self.size().height() == 300:
+            self.setGeometry(350, 273, 400, 300)       
+        event.accept()
+        
+    def show_about_us_text(self):
+        self.setGeometry(350, 273, 400, 500)
+        about_us_text = (
+            "\"Listen Your Emotion\" - это удивительное приложение, способное чувствовать и отражать ваше настроение через музыку. "
+            "Оно сканирует ваше лицо, захватывая каждое выражение и эмоцию, чтобы подобрать идеальный трек, который соответствует вашему текущему состоянию души.\n\n"
+            "С помощью \"Listen Your Emotion\" вы можете погрузиться в мир музыки, созданной специально для вас. От меланхоличных мелодий до бодрящих ритмов - приложение подберет идеальный саундтрек для каждого вашего настроения.\n\n"
+            "Позвольте себе окунуться в атмосферу гармонии и эмоций с \"Listen Your Emotion\" - приложением, которое делает вашу жизнь звучащей и красивой."
+        )
+        self.about_text_label.setText(about_us_text)
+        
+    def show_chat_support_text(self):
+        self.setGeometry(350, 273, 400, 100) 
+        self.about_text_label.setText("<a href='https://t.me/DatbyDay_bot'>Связаться с нами в Telegram</a>")
+        self.about_text_label.setOpenExternalLinks(True) 
+        
+    def show_rate_app_text(self):
+        self.setGeometry(350, 273, 400, 300) 
+        rate_app_text = "Пожалуйста, оцените наше приложение. Ваш отзыв очень важен для нас и поможет нам сделать его еще лучше!"
+        self.about_text_label.setText(rate_app_text)
 
 
 def emotion_to_number(emotion):
